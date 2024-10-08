6e73ed071c63c243d6e822052ccffe014be46f83
Slatebeard <max.ekberg@iths.se>
2024-10-08 17:06:29 +0200

    Add a test to RecipeBook

--- test/RecipeBookTest.java
+++ test/RecipeBookTest.java
@@ -1,6 +1,21 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+
 class RecipeBookTest {
-  
+
+
+
+
+
+    @Test
+    public void testAddRecipe() {
+        Recipe pancakes = new Recipie("Pancakes", "Flour", "Eggs", "Milk", "Mix ingredients and cook.");
+        recipeBook.addRecipe(pancakes);
+        assertEquals(1, recipeBook.getAllRecipes().size());
+    }
 }
