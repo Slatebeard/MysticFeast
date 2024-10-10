fcaae55e7fa32142456c17192d3adf02c30a36b2
Slatebeard <max.ekberg@iths.se>
2024-10-10 16:07:52 +0200

    Prelimanary error handeling

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -8,25 +8,42 @@ public class MainMenu {
 
 
     public MainMenu() {
-        System.out.println("Welcome to the main menu");
         runMenu();
     }
 
     public void runMenu() {
         // Variables
         boolean running = true;
+        boolean isPresent = false;
         int userChoice = sc.nextInt();
 
         // Objects
-        new RecipeBook();
+        RecipeBook recipeBook = new RecipeBook();
 
         while (running) {
             QOL.clearConsole();
             System.out.println(Art.intro);
 
 
+            //TODO Figure out a smart way to check for valid input
+
+            // Error Handling
+            int[] validChoices =  {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            if (userChoice == ){
+            }
+
+
             switch (userChoice) {
-                case 1 -> new 
+                case 1: {
+                    System.out.println(Art.getAllRecipes);
+                    recipeBook.getAllRecipes();
+                }
+                case 2: {
+                    System.out.println(Art.addRecipe);
+                    recipeBook.addRecipe();
+
+                }
+
 
             }
         }
